38b53b0f4aa5f7d782c861a2ea0209c53e3182a7
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-04-01 21:04:42 +0000

    latest

--- my-workspace/projects/my-app/src/app/app.component.spec.ts
+++ my-workspace/projects/my-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-first-app' title`, () => {
+  it(`should have the 'my-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('my-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
   });
 });
