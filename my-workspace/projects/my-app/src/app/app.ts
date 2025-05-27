617fbc4f3c652c9dc68fb3dc0c835926d6fb013a
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2025-05-27 17:01:17 +0000

    next

--- my-workspace/projects/my-app/src/app/app.ts
+++ my-workspace/projects/my-app/src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
-export class AppComponent {
-  title = 'my-app';
+export class App {
+  protected title = 'my-app';
 }
