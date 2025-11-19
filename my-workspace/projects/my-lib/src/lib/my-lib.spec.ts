a1254aee7407f124b4ffa32b8683b447c23600fa
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2025-11-19 20:18:53 +0000

    latest

--- my-workspace/projects/my-lib/src/lib/my-lib.spec.ts
+++ my-workspace/projects/my-lib/src/lib/my-lib.spec.ts
@@ -14,7 +14,7 @@ describe('MyLib', () => {
 
     fixture = TestBed.createComponent(MyLib);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    await fixture.whenStable();
   });
 
   it('should create', () => {
