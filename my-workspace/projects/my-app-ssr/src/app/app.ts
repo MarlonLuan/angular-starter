e0fc7af7f8935aade90d5b0b87f2453db1e37917
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2025-12-08 12:23:24 +0000

    latest

--- my-workspace/projects/my-app-ssr/src/app/app.ts
+++ my-workspace/projects/my-app-ssr/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = signal('my-app-ssr');
 }
