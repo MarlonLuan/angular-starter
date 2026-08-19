32cafd551e065086c3087f3e8d3fb2a656658bf9
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2026-08-19 20:20:03 +0000

    latest-plus

--- my-workspace/projects/my-app/src/app/app.ts
+++ my-workspace/projects/my-app/src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  selector: 'app-root',
   styleUrl: './app.css',
+  templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('my-app');
