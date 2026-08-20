82848bd560a6e3fc4746159a19ba0db51224be66
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2026-08-20 12:43:50 +0000

    latest

--- my-workspace/projects/my-lib/src/lib/my-lib.spec.ts
+++ my-workspace/projects/my-lib/src/lib/my-lib.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MyLib } from './my-lib';
 
 describe('MyLib', () => {
--- my-workspace/projects/my-lib/src/lib/my-lib.ts
+++ my-workspace/projects/my-lib/src/lib/my-lib.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-my-lib',
   imports: [],
-  template: ` <p>my-lib works!</p> `,
+  selector: 'lib-my-lib',
   styles: ``,
+  template: ` <p>my-lib works!</p> `,
 })
 export class MyLib {}
