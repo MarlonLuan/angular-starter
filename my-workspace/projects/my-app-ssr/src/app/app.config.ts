9a07c5ac0a69ea5fb09588a311933f72d360be30
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2026-08-28 23:55:39 +0000

    old

--- my-workspace/projects/my-app-ssr/src/app/app.config.ts
+++ my-workspace/projects/my-app-ssr/src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
   ],
 };
